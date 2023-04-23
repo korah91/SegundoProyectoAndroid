ba5f155c9afc9e3a4c694e4a367bb89127abe9b7
korah91 <62379054+korah91@users.noreply.github.com>
2023-04-23 18:48:59 +0200

    Actualizado el php

--- Archivos PHP COPIA SEGURIDAD/notificacionFirebase.php
+++ Archivos PHP COPIA SEGURIDAD/notificacionFirebase.php
@@ -1,50 +1,94 @@
 <?php
 
+// Cuando el profesor le da click al link del php se trata de una peticion GET
+// En este caso Firebase manda una notificacion a todos los dispositivos
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+	# Se declara la cabecera
+	$cabecera = array(
+	'Authorization:key=AAAAd403vNY:APA91bH2GdpWoP6ySOXaNMQecknr82g0XZQafx1kI6tjCCh5Dff0KbxRC2BE1HkJAcI3k8VGb4Ymqyf-98UMEHq_hQeRHAVEjTCfYxCg8yhjxkBbzpIzmb4ileNvwHG0VL2CfzAYeWe_',
+	'Content-Type:application/json'
+	);
 
-# Si se ha accedido con un post con el token del dispositivo hay que enviar 
-if(isset($_POST['token'])){
-    $token=$_POST['token'];
-    
-    # Se declara la cabecera
-    $cabecera = array(
-        'Authorization:key=AAAAd403vNY:APA91bH2GdpWoP6ySOXaNMQecknr82g0XZQafx1kI6tjCCh5Dff0KbxRC2BE1HkJAcI3k8VGb4Ymqyf-98UMEHq_hQeRHAVEjTCfYxCg8yhjxkBbzpIzmb4ileNvwHG0VL2CfzAYeWe_',
-        'Content-Type:application/json'
-    );
+	$msg= array(
+	'to'=>'/topics/ALERTS',
+	'notification'=> array(
+	    'body' => 'AVISO!! ULTIMA HORA!!',
+	    'title' => 'El admin les desea feliz navidad!!',
+	    'icon' => 'ic_stat_ic_notification',
+	    'click_action'=>"AVISO")
+	);
+	# Se convierte a JSON
+	$msgJSON= json_encode($msg);
 
-    $msg= array(
-        'to'=>$token,
-        'notification'=> array(
-            'body' => 'Usaremos tu imagen de la universidad para ayudar a más gente',
-            'title' => '¡Gracias por tu imagen!',
-            'icon' => 'ic_stat_ic_notification',
-            'click_action'=>"AVISO")
-        );
-    # Se convierte a JSON
-    $msgJSON= json_encode($msg);
+	$ch = curl_init(); #inicializar el handler de curl
+	#indicar el destino de la petición, el servicio FCM de google
+	curl_setopt( $ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+	#indicar que la conexión es de tipo POST
+	curl_setopt( $ch, CURLOPT_POST, true );
+	#agregar las cabeceras
+	curl_setopt( $ch, CURLOPT_HTTPHEADER, $cabecera);
+	#Indicar que se desea recibir la respuesta a la conexión en forma de string
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+	#agregar los datos de la petición en formato JSON
+	curl_setopt( $ch, CURLOPT_POSTFIELDS, $msgJSON );
+	#ejecutar la llamada
+	$resultado= curl_exec( $ch );
+	#cerrar el handler de curl
+	curl_close( $ch );
 
-    $ch = curl_init(); #inicializar el handler de curl
-    #indicar el destino de la petición, el servicio FCM de google
-    curl_setopt( $ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-    #indicar que la conexión es de tipo POST
-    curl_setopt( $ch, CURLOPT_POST, true );
-    #agregar las cabeceras
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, $cabecera);
-    #Indicar que se desea recibir la respuesta a la conexión en forma de string
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-    #agregar los datos de la petición en formato JSON
-    curl_setopt( $ch, CURLOPT_POSTFIELDS, $msgJSON );
-    #ejecutar la llamada
-    $resultado= curl_exec( $ch );
-    #cerrar el handler de curl
-    curl_close( $ch );
+	echo $resultado;
+	echo "Se ha mandado un mensaje a todos los moviles";
+}
 
-    if (curl_errno($ch)) {
-        print curl_error($ch);
-        }
-        echo $resultado;}
-    else{
-        echo $resultado;
-    }
-    echo $cabecera[0];
+// Si se trata de una peticion POST es una peticion desde la aplicacion. Se manda una notificacion al dispositivo que ha mandado el POST
+else{
+
+	# Si se ha accedido con un post con el token del dispositivo hay que enviar
+	if(isset($_POST['token'])){
+	    $token=$_POST['token'];
+
+	    # Se declara la cabecera
+	    $cabecera = array(
+		'Authorization:key=AAAAd403vNY:APA91bH2GdpWoP6ySOXaNMQecknr82g0XZQafx1kI6tjCCh5Dff0KbxRC2BE1HkJAcI3k8VGb4Ymqyf-98UMEHq_hQeRHAVEjTCfYxCg8yhjxkBbzpIzmb4ileNvwHG0VL2CfzAYeWe_',
+		'Content-Type:application/json'
+	    );
+
+	    $msg= array(
+		'to'=>$token,
+		'notification'=> array(
+		    'body' => 'Usaremos tu imagen de la universidad para ayudar a más gente',
+		    'title' => '¡Gracias por tu imagen!',
+		    'icon' => 'ic_stat_ic_notification',
+		    'click_action'=>"AVISO")
+		);
+	    # Se convierte a JSON
+	    $msgJSON= json_encode($msg);
+
+	    $ch = curl_init(); #inicializar el handler de curl
+	    #indicar el destino de la petición, el servicio FCM de google
+	    curl_setopt( $ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+	    #indicar que la conexión es de tipo POST
+	    curl_setopt( $ch, CURLOPT_POST, true );
+	    #agregar las cabeceras
+	    curl_setopt( $ch, CURLOPT_HTTPHEADER, $cabecera);
+	    #Indicar que se desea recibir la respuesta a la conexión en forma de string
+	    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+	    #agregar los datos de la petición en formato JSON
+	    curl_setopt( $ch, CURLOPT_POSTFIELDS, $msgJSON );
+	    #ejecutar la llamada
+	    $resultado= curl_exec( $ch );
+	    #cerrar el handler de curl
+	    curl_close( $ch );
+
+	    if (curl_errno($ch)) {
+		print curl_error($ch);
+		}
+		echo $resultado;}
+	    else{
+		echo $resultado;
+	    }
+
+
+}
 
 ?>
